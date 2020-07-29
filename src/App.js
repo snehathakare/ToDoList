2b90d86e3f24ef86715fa270472565f8d2dc1986
Sneha <snhthakare@gmail.com>
2020-07-29 22:10:42 +0530

    Adds button and text field events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, {useState} from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
 
-  const [todos, setTodos] = useState(["Cook Dinner", "Go for a walk", "Commit code to git"]);
+  const [todos, setTodos] = useState(['']);
+  const [input, setInput] = useState('');
+  
+
+  const addToDo = (event) => {
+  	event.preventDefault();
+  	setTodos([...todos, input]);
+  	setInput('');
+  }
 
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained" color="primary">
+      <input value={input} id="outlined-basic" label="Outlined" variant="outlined" onChange={event => setInput(event.target.value)} />
+      <Button variant="contained" color="primary" onClick={addToDo}>
   		Add Task
 	  </Button>
 	  <ul>
-	  	{todos.map(todos => (
+	  	{
+	  		todos.map(todos => (
 	  		<li>{todos}</li>
-
 	  		)
 	  	)}
 	  	
